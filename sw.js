66047861059487183fc57b7db8299baecc3864ce
unknown <farhandian2124@gmail.com>
2026-07-09 15:47:03 +0800

    update

--- sw.js
+++ sw.js
@@ -1,35 +1,24 @@
-const CACHE_NAME = 'tranzivo-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/kasir.js',
-  '/manifest.webmanifest',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
-  ,'/icons/logotrazivo.png'
-  ,'/icons/logotrazivo.svg'
+const CACHE = "tranzivo-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./kasir.js",
+    "./manifest.webmanifest",
+    "./icons/logotrazivo-192.png",
+    "./icons/logotrazivo-512.png"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(FILES))
+    );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request).then(r => {
-      return caches.open(CACHE_NAME).then(cache => { cache.put(event.request, r.clone()); return r; });
-    })).catch(()=>caches.match('/index.html'))
-  );
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request);
+        })
+    );
 });
